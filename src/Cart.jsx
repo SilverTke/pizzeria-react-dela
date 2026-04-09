cc505c5a616bec011826a31771dbaf7ae1b12370
Sofía Maturana <sofimaturana830@gmail.com>
2026-04-09 15:04:24 -0400

    feat: agregar imágenes al carrito

--- src/Cart.jsx
+++ src/Cart.jsx
@@ -15,38 +15,41 @@ const Cart = () => {
       <div id="cartContainer" className="flex gap-4 flex-col">
         {cart.map((pizza) => (
           <div
-            className="border-3 border-lime-300 rounded-md p-2"
+            className="border-3 border-lime-300 rounded-md flex gap-4 overflow-hidden"
             key={pizza.id}
           >
-            <h2>Pizza {pizza.name}</h2>
-            <div className="flex gap-4">
-              <button
-                onClick={() => {
-                  setCart((cart) =>
-                    cart.map((p) =>
-                      p.id === pizza.id ? { ...p, count: p.count + 1 } : p,
-                    ),
-                  );
-                }}
-                className="bg-blue-500 px-4 text-white rounded-md"
-              >
-                +
-              </button>
-              <p className="text-grey-500">{pizza.count}</p>
-              <button
-                onClick={() => {
-                  setCart((cart) =>
-                    cart
-                      .map((p) =>
-                        p.id === pizza.id ? { ...p, count: p.count - 1 } : p,
-                      )
-                      .filter((p) => p.count > 0),
-                  );
-                }}
-                className="bg-red-500 px-4 text-white rounded-md"
-              >
-                -
-              </button>
+            <img src={pizza.img} className="w-32 rounded-r-sm" />
+            <div>
+              <h2>Pizza {pizza.name}</h2>
+              <div className="flex gap-4">
+                <button
+                  onClick={() => {
+                    setCart((cart) =>
+                      cart.map((p) =>
+                        p.id === pizza.id ? { ...p, count: p.count + 1 } : p,
+                      ),
+                    );
+                  }}
+                  className="bg-blue-500 px-4 text-white rounded-md"
+                >
+                  +
+                </button>
+                <p className="text-grey-500">{pizza.count}</p>
+                <button
+                  onClick={() => {
+                    setCart((cart) =>
+                      cart
+                        .map((p) =>
+                          p.id === pizza.id ? { ...p, count: p.count - 1 } : p,
+                        )
+                        .filter((p) => p.count > 0),
+                    );
+                  }}
+                  className="bg-red-500 px-4 text-white rounded-md"
+                >
+                  -
+                </button>
+              </div>
             </div>
           </div>
         ))}
